7d0715ddfaee25659cc6ee71113badff9f6933eb
Alexander Ziskind <alex@nuvious.com>
2019-05-07 14:46:54 -0400

    added demo-destroy

---
+++ demo-destroy/src/app/item/items.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, HostListener, OnDestroy } from "@angular/core";
+
+import { Item } from "./item";
+import { ItemService } from "./item.service";
+
+@Component({
+    selector: "ns-items",
+    moduleId: module.id,
+    templateUrl: "./items.component.html"
+})
+export class ItemsComponent implements OnInit, OnDestroy {
+    items: Array<Item>;
+
+    // This pattern makes use of Angular’s dependency injection implementation to
+    // inject an instance of the ItemService service into this class.
+    // Angular knows about this service because it is included in your app’s main NgModule,
+    // defined in app.module.ts.
+    constructor(private itemService: ItemService) { }
+
+    ngOnInit(): void {
+        this.items = this.itemService.getItems();
+    }
+
+    @HostListener('unloaded')
+    ngOnDestroy() {
+        console.log('items destroyed');
+    }
+}
